7476c7942fc645964cc972c3efb25323b5f3d1d1
samkillah <samanthacas29@gmail.com>
2022-11-10 22:33:49 -0500

    frontend, conexion registro

---
+++ extraclase/frontendlogin/Login-y-registro-con-html-css-js/php/conexion_backend.php
@@ -0,0 +1,19 @@
+<?php
+
+$conexion= mysqli_connect("localhost", "root", "", "login_register_db");
+
+/*
+if($conexion){
+    echo 'conectado existosamente a la base de datos';
+}else{
+    echo 'no se ha podido conectar a la base de datos';
+}
+*/
+
+
+
+
+
+
+
+?>
